c1b29c9dc6dcea68839044b281bdf8646adedd98
Ville Kurimo <wilhelmo313@gmail.com>
2019-08-29 20:55:30 +0300

    Update App.js

--- App.js
+++ App.js
@@ -35,22 +35,22 @@ const App = () => {
   return (
     <View style={styles.container}>
       <FlatList
-    data={mediaArray}
-    renderItem={({item}) => {
-      return (
-        <TouchableOpacity>
-          <Image
-            style={{width: 100, height: 100}}
-            source={{uri: item.thumbnails.w160}}
-          />
-          <View>
-            <Text>{item.title}</Text>
-            <Text>{item.description}</Text>
-          </View>
-        </TouchableOpacity>
-      );
-    }}
-  />
+        data={mediaArray}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity>
+              <Image
+                style={{width: 100, height: 100}}
+                source={{uri: item.thumbnails.w160}}
+              />
+              <View>
+                <Text>{item.title}</Text>
+                <Text>{item.description}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
