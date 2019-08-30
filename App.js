fc9d4905e87246fe9d8634ab330f4244f1b542fa
Ville Kurimo <wilhelmo313@gmail.com>
2019-08-30 11:18:03 +0300

    .

--- App.js
+++ App.js
@@ -1,68 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
-const mediaArray = [
-  {
-    'key': '0',
-    'title': 'Title 1',
-    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/161',
-    },
-    'filename': 'http://placekitten.com/2048/1920',
-  },
-  {
-    'key': '1',
-    'title': 'Title 2',
-    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/162',
-    },
-    'filename': 'http://placekitten.com/2041/1922',
-  },
-  {
-    'key': '2',
-    'title': 'Title 3',
-    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/163',
-    },
-    'filename': 'http://placekitten.com/2039/1920',
-  },
-];
+import {StyleSheet, View, Text} from 'react-native';
+import List from './components/List';
+import {MediaProvider} from './components/contexts/MediaContext';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={mediaArray}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity>
-              <Image
-                style={{width: 100, height: 100}}
-                source={{uri: item.thumbnails.w160}}
-              />
-              <View>
-                <Text>{item.title}</Text>
-                <Text>{item.description}</Text>
-              </View>
-            </TouchableOpacity>
-          );
-        }}
-      />
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MediaProvider>
+      <View style={styles.container}>
+        <Text style={styles.header}>{'CATS'}</Text>
+        <List />
+      </View>
+    </MediaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    paddingTop: 15,
+    paddingLeft: 15,
   },
 });
-
 export default App;
